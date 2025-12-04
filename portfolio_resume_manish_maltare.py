810d13847be4f64cbac0fac4601e0aa7bb05817b
manishmaltare <manishmaltare@gmail.com>
2025-12-04 21:47:51 +0530

    Update portfolio_resume_manish_maltare.py

--- portfolio_resume_manish_maltare.py
+++ portfolio_resume_manish_maltare.py
@@ -256,7 +256,23 @@ if menu == "About Me":
     st.markdown("<div class='main-title'>Manish Maltare</div>", unsafe_allow_html=True)
     st.markdown("<div class='sub-title-tagline'>Digital Portfolio</div>", unsafe_allow_html=True)
     st.markdown("<div class='section-title'>About Me</div>", unsafe_allow_html=True)
-    st.write(about_text)
+
+    # Wrap About Me text in a semi-transparent black box
+    st.markdown(
+        f"""
+        <div style="
+            background-color: rgba(0,0,0,0.6);
+            padding: 20px;
+            border-radius: 10px;
+            color: white;
+            line-height: 1.6;
+        ">
+            {about_text.replace('\n','<br>')}
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
 
 elif menu == "Projects":
     st.markdown('<a id="projects"></a>', unsafe_allow_html=True)
